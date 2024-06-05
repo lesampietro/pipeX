d00a8c9b283098351700d3549d119f407d54c240
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-06-05 13:22:53 -0300

    check cmd function ok but valgrind stills hs errors

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 19:48:16 by lsampiet          #+#    #+#             */
-/*   Updated: 2024/06/02 22:42:26 by lsampiet         ###   ########.fr       */
+/*   Updated: 2024/06/05 13:21:02 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	check_args(int argc)
 {
-	if (argc != 4)
+	if (argc != 3)
 	{
 		perror("Error: Wrong number of arguments\n");
 		exit(1);
@@ -30,82 +30,88 @@ void	exec_cmd(char *cmd, char **argv, char **envp)
 	}
 }
 
-void	check_cmd(char **argv, char **envp)
+char	*check_cmd(char *argv, char **envp)
 {
 	int		i;
-	int		j;
 	char	*path;
 	char	**paths;
 	char	*cmd;
 
 	i = 0;
-	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
 		i++;
-	if (!envp[i])
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
 	{
-		perror("Error: PATH not found\n");
-		exit(1);
-	}
-	path = ft_strchr(envp[i], '=') + 1;
-	paths = ft_split(path, ':');
-	j = 0;
-	while (paths[j])
-	{
-		paths[j] = ft_strjoin(paths[j], "/");
-		cmd = ft_strjoin(paths[j], argv[2]);
+		path = ft_strjoin(paths[i], "/");
+		cmd = ft_strjoin(path, argv);
+		free(path);
 		if (access (cmd, X_OK | F_OK) == 0)
-		{	
-			exec_cmd(cmd, &argv[2], envp);
-			return ;
-		}
+			return (cmd);
 		free(cmd);
-		free(paths[j]);
-		j++;
+		i++;
 	}
-	perror("Error: Invalid command\n");
-	exit(1);
+	while (paths[i])
+		free(paths[i--]);
+	free(paths);
+	return (0);
 }
+
+// perror("Error: Invalid command\n");
+// exit(1);
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		fd[2];
-	t_pipex	data;
+	// int		fd[2];
+	// t_pipex	data;
 
 	check_args(argc);
-	data.fd_in = open(argv[1], O_RDONLY); 
-	if (data.fd_in < 0)
-	{
-		perror("Error: Invalid input file\n");
-		exit(1);
-	}
-	data.fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (data.fd_out < 0)
-	{
-		perror("Error: Invalid output file\n");
-		close(data.fd_in);
-		exit(1);
-	}
-	pipe(fd);
-	data.pid = fork();
-	if (data.pid == 0)
-	{
-		dup2(data.fd_in, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		close(data.fd_in);
-		close(data.fd_out);
-		check_cmd(argv, envp);
-	}
-	else
-	{
-		dup2(fd[0], STDIN_FILENO);
-		dup2(data.fd_out, STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		close(data.fd_in);
-		close(data.fd_out);
-		wait(NULL);
-	}
-return (0);
+	check_cmd(argv[2], envp);
+	// data.fd_in = open(argv[1], O_RDONLY); 
+	// if (data.fd_in < 0)
+	// {
+	// 	perror("Error: Invalid input file\n");
+	// 	exit(1);
+	// }
+	// data.fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	// if (data.fd_out < 0)
+	// {
+	// 	perror("Error: Invalid output file\n");
+	// 	close(data.fd_in);
+	// 	exit(1);
+	// }
+	// pipe(fd);
+	// data.pid = fork();
+	// if (data.pid == 0)
+	// {
+	// 	dup2(data.fd_in, STDIN_FILENO);
+	// 	dup2(fd[1], STDOUT_FILENO);
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+	// 	close(data.fd_in);
+	// 	close(data.fd_out);
+	// }
+// 	else
+// 	{
+// 		dup2(fd[0], STDIN_FILENO);
+// 		dup2(data.fd_out, STDOUT_FILENO);
+// 		close(fd[0]);
+// 		close(fd[1]);
+// 		close(data.fd_in);
+// 		close(data.fd_out);
+// 		wait(NULL);
+// 	}
+	return (0);
 }
+
+// Function execute
+// exec_cmd(cmd, &argv[2], envp);
+
+
+// Check for envp
+// if (!envp[i])
+// {
+// 	perror("Error: PATH not found\n");
+// 	exit(1);
+// }
